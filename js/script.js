2fd166611eba45c7b796e3bff76e17c21249e51b
Dmitry Fedoseev <fedoseevdmitry0@gmail.com>
2022-10-01 07:10:53 +0300

    complete 5-2

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const itemTitleName = document.querySelector('.modal__title');
+const itemIdNumber = document.querySelector('.id__number');
+const itemIdBtn = document.querySelector('.id__btn');
+const itemForm = document.querySelector('.modal__form');
+const itemCheckbox = document.querySelector('.fieldset__input_checkbox');
+const itemCheckboxValue = document.querySelector('.fieldset__input_checkbox-input');
+const itemPrice = document.querySelector('.modal__text_bold');
+
+console.log(itemTitleName);
+console.log(itemIdNumber);
+console.log(itemIdBtn);
+console.log(itemForm);
+console.log(itemCheckbox);
+console.log(itemCheckboxValue);
+console.log(itemPrice);
